28494a6f12aa9c544c8d5f2c410d8bb9900e709d
Guilherme Kameoka <guilherme.kameoka@outlook.com>
2024-06-17 23:20:33 -0300

    first commit

---
+++ src/discord/events/logs/onLeaveServer.ts
@@ -0,0 +1,30 @@
+import { Event } from "@/discord/base";
+import { brBuilder } from "@/functions/utils/format";
+import { hexToRgb } from "@/functions/utils/convert";
+import { settings } from "@/settings";
+import{ EmbedBuilder, TextChannel, time} from "discord.js";
+
+const channelId = "1252440973074497587";
+
+export default new Event({
+    name: "guildMemberRemove",
+    run(member) {
+        const channel = member.guild.channels.cache.get(channelId) as TextChannel;
+        const memberAvatarUrl = member.displayAvatarURL({ size: 512 });
+
+        channel.send({
+            embeds: [new EmbedBuilder({
+                color: hexToRgb(settings.colors.theme.danger),
+                author: {
+                    name: `${member.displayName} saiu do servidor!`,
+                    iconURL: memberAvatarUrl
+                },
+                thumbnail: {url: memberAvatarUrl},
+                description: brBuilder(
+                    `👀 ${member} acabou de sair do servidor`,
+                    time(new Date(), "f")
+                )
+            })]
+         });
+    },
+});
